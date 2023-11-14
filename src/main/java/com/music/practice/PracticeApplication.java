c7f81433bbc6c74aa39de4c11d92615792021c1b
hagenzhang <64102519+hagenzhang@users.noreply.github.com>
2023-11-14 07:55:30 -0500

    updated to most recent version

--- src/main/java/com/music/practice/PracticeApplication.java
+++ src/main/java/com/music/practice/PracticeApplication.java
@@ -2,11 +2,16 @@ package com.music.practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PracticeApplication {
+public class PracticeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
+		// Refer to these tutorials for SpringAPI hosting:
+		// - https://tanzu.vmware.com/developer/guides/serverless-spring/
+		// - https://www.youtube.com/watch?v=GGPPkUcHleQ
+		//   ^^ I currently use this approach, with AWS API Gateway and AWS Lambda
 		SpringApplication.run(PracticeApplication.class, args);
 	}
 
